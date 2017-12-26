47b14851ca969fdf127bb1445117bfc39721c9c4
Nishisonic <graphic.extra005@gmail.com>
2017-12-26 22:06:28 +0900

    ES6実装が上手くいっていないため、ES5に即した形に書き換え

--- drop_unexpectedDamage.js
+++ drop_unexpectedDamage.js
@@ -1,6 +1,6 @@
 /**
  * 異常ダメージ検知
- * @version 1.0.1
+ * @version 1.0.2
  * @author Nishisonic
  */
 ComparableArrayType = Java.type("java.lang.Comparable[]")
@@ -292,33 +292,10 @@ ShipHpDto.prototype.copy = function () {
  * @return {Battle} 既定のフォーマット
  */
 var parse = function (date, phaseList, friendNum, friendNumCombined, enemyNum, enemyNumCombined, friendCombinedKind, isEnemyCombined) {
-    // fill
-    if (!Array.prototype.fill) {
-        Object.defineProperty(Array.prototype, 'fill', {
-            value: function (value) {
-                if (this == null) {
-                    throw new TypeError('this is null or not defined')
-                }
-                var O = Object(this)
-                var len = O.length >>> 0
-                var start = arguments[1]
-                var relativeStart = start >> 0
-                var k = relativeStart < 0 ? Math.max(len + relativeStart, 0) : Math.min(relativeStart, len)
-                var end = arguments[2]
-                var relativeEnd = end === undefined ? len : end >> 0
-                var final = relativeEnd < 0 ? Math.max(len + relativeEnd, 0) : Math.min(relativeEnd, len)
-                while (k < final) {
-                    O[k++] = value
-                }
-                return O
-            }
-        })
-    }
-    var touchPlaneList = []
-    var airDamagedFriendHp = new DamagedHpDto(new Array(friendNum).fill(0), new Array(friendNumCombined).fill(0))
-    var airDamagedEnemyHp = new DamagedHpDto(new Array(enemyNum).fill(0), new Array(enemyNumCombined).fill(0))
-    var supportDamagedFriendHp = new DamagedHpDto(new Array(friendNum).fill(0), new Array(friendNumCombined).fill(0))
-    var supportDamagedEnemyHp = new DamagedHpDto(new Array(enemyNum).fill(0), new Array(enemyNumCombined).fill(0))
+    var airDamagedFriendHp = new DamagedHpDto(Array.apply(null, { length: friendNum }).map(function (v) { return 0 }), Array.apply(null, { length: friendNumCombined }).map(function (v) { return 0 }))
+    var airDamagedEnemyHp = new DamagedHpDto(Array.apply(null, { length: enemyNum }).map(function (v) { return 0 }), Array.apply(null, { length: enemyNumCombined }).map(function (v) { return 0 }))
+    var supportDamagedFriendHp = new DamagedHpDto(Array.apply(null, { length: friendNum }).map(function (v) { return 0 }), Array.apply(null, { length: friendNumCombined }).map(function (v) { return 0 }))
+    var supportDamagedEnemyHp = new DamagedHpDto(Array.apply(null, { length: enemyNum }).map(function (v) { return 0 }), Array.apply(null, { length: enemyNumCombined }).map(function (v) { return 0 }))
     var dayKind = null
     var nightKind = null
     var nightBattle1 = null
